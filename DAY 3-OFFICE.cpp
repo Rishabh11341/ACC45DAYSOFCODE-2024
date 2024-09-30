a65adf7b42ad0904d65c3492456283d532f4922f
Rishabh11341 <145369091+Rishabh11341@users.noreply.github.com>
2024-09-30 06:23:59 -0700

    Add files via upload

---
+++ DAY 3-OFFICE.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+	while(t--){
+	    int x;
+	    int y;
+	    cin>>x>>y;
+	    int totalhours;
+	    totalhours=x*4+y;
+	    cout<<totalhours<<endl;
+	}
+return 0;
+}
