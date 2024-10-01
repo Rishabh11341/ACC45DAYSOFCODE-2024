7cccc84a3919cf5398fd2e98d8c7347a1834b101
Rishabh11341 <145369091+Rishabh11341@users.noreply.github.com>
2024-10-01 10:16:18 -0700

    Add files via upload

---
+++ DAY 4-THREETOPICS.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	int c;
+	int x;
+	cin>>a>>b>>c>>x;
+	
+	if(a==x){
+	    cout<<"yes"<<endl;
+	}
+	else if(b==x){
+	    cout<<"yes"<<endl;
+	}
+	else if(c==x){
+	    cout<<"yes"<<endl;
+	}
+	else{
+	    cout<<"no"<<endl;
+	}
+return 0;
+}
