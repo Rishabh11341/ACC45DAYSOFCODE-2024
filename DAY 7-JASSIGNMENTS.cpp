33d94ba89516c0801267d4305b81465191e422bc
Rishabh11341 <145369091+Rishabh11341@users.noreply.github.com>
2024-10-04 09:37:22 -0700

    Add files via upload

---
+++ DAY 7-JASSIGNMENTS.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if((10-x)>=3){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
