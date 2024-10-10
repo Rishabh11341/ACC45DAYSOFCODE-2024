f20908509256a079f49e7b9d31194d218b868953
Rishabh11341 <145369091+Rishabh11341@users.noreply.github.com>
2024-10-10 09:49:31 -0700

    Add files via upload

---
+++ DAY 13-EXPERT.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        int y;
+        cin>>x>>y;
+        if(2*y>=x){
+            std::cout << "YES" << std::endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+return 0;
+}
